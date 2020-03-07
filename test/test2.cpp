304d671fb01500d7536e55548d3aa26f23a0e83b
Caian Benedicto <caianbene@gmail.com>
2020-03-07 10:11:49 -0300

    Fix license headers

--- test/test2.cpp
+++ test/test2.cpp
@@ -1,21 +1,20 @@
 /*
  * Copyright (C) 2019 Caian Benedicto <caianbene@gmail.com>
  *
- * This file is part of historian.
+ * This file is part of h1st.
  *
- * historian is free software; you can redistribute it and/or modify
+ * h1st is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2, or (at your option)
  * any later version.
  *
- * historian is distributed in the hope that it will be useful,
+ * h1st is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with GNU Make; see the file COPYING.  If not, write to
- * the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+ * along with h1st.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include <h1st/historian.hpp>
